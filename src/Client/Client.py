37d29de21dc10b76072961ddbe76494711a433df
Maciek <maciek.wojtowicz97@gmail.com>
2020-12-28 00:46:51 +0100

    Added creating daemon process and epoll.

--- src/Client/Client.py
+++ src/Client/Client.py
@@ -4,29 +4,48 @@ import sys
 import os
 
 BUFFER_SIZE=4096
-class Client:
-    def __init__(self,ip_address,port,file_name):
-        self.ip_address=ip_address
-        self.port=port
-        self.file_name=file_name
+PORT=5000
 
-    def start(self):
-        client_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        client_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-        print(f"Server address : {self.ip_address} file to send : {self.file_name}")
-        client_socket.connect((self.ip_address,self.port))
-        size=os.path.getsize(self.file_name)
-        print(f"Message {self.file_name}:{size}")
-        client_socket.send(f"{self.file_name}:{size}".encode())
-        reading=True
-        with open(self.file_name,"rb") as file:
-            while reading:
+def read_and_send(client_socket,filename):
+     with open(filename,"rb") as file:
+            while True:
                 byte_read=file.read(BUFFER_SIZE)
                 if not byte_read:
                     reading=False
                     break
                 client_socket.sendall(byte_read)
             
+
+class Client:
+    def __init__(self,ip_address,port,file_name):
+        self.ip_address=ip_address
+        self.port=port
+        self.file_name=file_name
+
+    def socket_error_handler(self,exception_msg,exception_place,fd_socket):
+        print("{0} : {1}".format(exception_place,exception_msg))
+        sys.exit(1)
+
+    def start(self):
+        try:
+            client_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        except socket.error as msg:
+            self.socket_error_handler(msg,'Client_start()',client_socket)
+        try:
+            client_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+        except socket.error as msg:
+            self.socket_error_handler(msg,'Client_start()',client_socket)
+        print(f"Server address : {self.ip_address} file to send : {self.file_name}")
+        try:
+            client_socket.connect((self.ip_address,self.port))
+        except socket.error as msg:
+            self.socket_error_handler(msg,'Client_start()',client_socket)
+        print(f"Message {self.file_name}")
+        try:
+            client_socket.send(f"{self.file_name}".encode())
+        except socket.error as msg:
+            self.socket_error_handler(msg,'Client_start()',client_socket)
+        read_and_send(client_socket,self.file_name)
         client_socket.close()
 
 
@@ -34,5 +53,5 @@ class Client:
 if __name__ == "__main__":
     print(f"Script arugment: {sys.argv[1]}")
     print(f"File to send : {sys.argv[2]}")
-    first_Client=Client(sys.argv[1],5000,sys.argv[2])
+    first_Client=Client(sys.argv[1],PORT,sys.argv[2])
     first_Client.start()
--- src/Server/Server.py
+++ src/Server/Server.py
@@ -1,9 +1,14 @@
 import socket
 import logging
 import os
+import sys
+import time
+import atexit
+import select
 
-SERVER_PORT=5000
+PORT=5000
 BUFFER_SIZE=4096
+
 def get_logger_file(name,file_path,log_level):
     log_file=logging.getLogger(name)
     log_file.setLevel(log_level)
@@ -13,58 +18,167 @@ def get_logger_file(name,file_path,log_level):
     log_file.addHandler(handler)
     return log_file
 
-def socket_error_handler(exception_msg,exception_place,fd_socket,logger):
-    logger.error("%s : %s".format(exception_place,exception_msg))
-
-
-class Server:
-    def __init__(self,ip_address,port,log_file):
-        self.ip_address=ip_address
-        self.port=port
-
-    def start(self):
-        log_file.info("Server started")
-        try:
-            log_file.debug("Server_start(): Create socket")
-            fd_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        except socket.error as msg:
-            socket_error_handler(msg,'Server_start()',fd_socket,log_file)
-        try:
-            log_file.debug("Server_start(): set socket option")
-            fd_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-        except socket.error as msg:
-            socket_error_handler(msg,'Server_start()',fd_socket,log_file)
-        try:
-            log_file.debug("Server_start(): socket bind ")
-            fd_socket.bind((self.ip_address,self.port))
-        except socket.error as msg:
-            socket_error_handler(msg,'Server_start()',fd_socket,log_file)
-        fd_socket.listen(3)
-        client_socket, address=fd_socket.accept()
-        print(f"{address} is connected.")
-        received=client_socket.recv(BUFFER_SIZE).decode()
-        filename,filesize=received.split(":")
-        filename=os.path.basename(filename)
-        filesize=int(filesize)
-        reading=True
-        with open(filename,"wb") as file:
-            while reading :
+def recv_file(client_socket,filename):
+    with open(filename,"wb") as file:
+            while True :
                 byte_read=client_socket.recv(BUFFER_SIZE)
                 if not byte_read:
                     reading=False
                     break
                 file.write(byte_read)
-        client_socket.close()
-        log_file.debug("Server_start(): ended download from client")
-        fd_socket.close()
+class Daemon:
+    def __init__(self,pidfile):
+        self.pidfile=pidfile
 
+    def daemonize(self):
+        # Fork and exit first parent process
+        try:
+            pid=os.fork()
+            if pid>0:
+                sys.exit(0)
+        except OSError as err:
+            sys.stderr.write('fork #1 failed: {0}\n'.format(err))
+            sys.exit(1)
 
+        # disconnect from parent environment
+        # os.chdir("/")
+        os.setsid()
+        os.umask(0)
+
+        # Second fork
+        try:
+            pid=os.fork()
+            if pid>0:
+                sys.exit(0)
+        except OSError as err:
+            sys.stderr.write('fork #2 failed: {0}\n' .format(err))
+            sys.exit(1)
+        
+        # Redirect standard files in/out/err
+        sys.stdout.flush()
+        sys.stderr.flush()
+        si = open(os.devnull,'r')
+        so = open(os.devnull,'a+')
+        se = open(os.devnull, 'a+')
+        os.dup2(si.fileno(), sys.stdin.fileno())
+        os.dup2(so.fileno(), sys.stdout.fileno())
+        os.dup2(se.fileno(), sys.stderr.fileno())
+        
+        # write pidfile
+        atexit.register(self.delpid)
+        pid = str(os.getpid())
+        with open(self.pidfile,'w+') as file:
+            file.write(pid+'\n')
+    
+    def delpid(self):
+        os.remove(self.pidfile)
+
+    def start(self):
+        # Check for a pidfile to see if deamon already runs
+        try:
+            with open(self.pidfile,'r') as pfile:
+                pid = int(pfile.read().strip())
+            
+        except IOError:
+            pid = None
+        if pid:
+            message = "pidfile {0} already exist." + \
+                "Daemon already running?\n"
+            sys.stderr.write(message.format(self.pidfile))
+            sys.exit(1)
+
+        self.daemonize()
+        self.run()
+
+    def run(self):
+        raise NotImplementedError
+
+class Server(Daemon):
+    def __init__(self,ip_address,port):
+        super().__init__('/tmp/deamon-test.pid')
+        self.ip_address=ip_address
+        self.port=port
+        self.log_file=get_logger_file("log_operations","/var/log/server.log",logging.DEBUG)
+    
+    def socket_error_handler(exception_msg,exception_place,fd_socket):
+        self.log_file.error("%s : %s".format(exception_place,exception_msg))
+        sys.exit(1)
+
+    def run(self):
+        self.log_file.info("Server_run(): Server started")
+        try:
+            self.log_file.debug("Server_run(): Create socket")
+            fd_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        except socket.error as msg:
+            socket_error_handler(msg,'Server_run()',fd_socket,log_file)
+        try:
+            self.log_file.debug("Server_run(): set socket option")
+            fd_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+        except socket.error as msg:
+            socket_error_handler(msg,'Server_start()',fd_socket,log_file)
+        try:
+            self.log_file.debug("Server_run(): socket bind ")
+            fd_socket.bind((self.ip_address,self.port))
+        except socket.error as msg:
+            socket_error_handler(msg,'Server_run()',fd_socket,log_file)
+        fd_socket.listen(3)
+        # setting server socket nonblocking 
+        fd_socket.setblocking(False)
+
+        # epoll object creation
+        epoll = select.epoll()
+        epoll.register(fd_socket.fileno(),select.EPOLLIN | select.EPOLLOUT)
+
+        try: 
+            connections={}
+            requests={}
+            responses={}
+            firstMessage={}
+            while True:
+                # waiting up to 30 seconds for event to occure and return query epoll object 
+                events = epoll.poll(30)
+                for fileno, event in events:
+                    # if event occurse on the server socket then a new socket connection show up
+                    if fileno == fd_socket.fileno():
+                        self.log_file.info("Server_run(): new connection found")
+                        connection, address = fd_socket.accept()
+                        connection.setblocking(False)
+                        epoll.register(connection.fileno(),select.EPOLLIN | select.EPOLLOUT)
+                        connections[connection.fileno()] = connection
+                        requests[connection.fileno()] =''
+                        responses[connection.fileno()] =''
+                        firstMessage[connection.fileno()] = True
+                        self.log_file.info("Server_run(): connection {0} added to epoll".format(address))
+                    elif event & select.EPOLLIN:
+                        # read data because EPOLLIN event occured
+                        if firstMessage[fileno] == True :
+                            filename=connections[fileno].recv(BUFFER_SIZE).decode()
+                            self.log_file.info("Server_run(): received message/data - {0}".format(filename))
+                            filename=os.path.basename(filename)
+                            self.log_file.info("Server_run(): download file {0}".format(filename))
+                            recv_file(connections[fileno],filename)
+                            self.log_file.info("Server_run(): fileno = {0} ended downloading file {1}".format(fileno,filename))
+                            firstMessage[fileno] = False
+                    elif event & select.EPOLLOUT:
+                        # write data because EPOLLOUT event occured
+                        byteswritten = connections[fileno].send(responses[fileno])
+                        responses[fileno] = responses[fileno][byteswritten:]
+                        self.log_file.info("Server_run(): write message/data - {0}".format(responses[fileno]))
+                    elif event & select.EPOLLHUP:
+                        # delete socket connection from epoll because client hang up
+                        epoll.unregister(fileno)
+                        self.log_file.info("Server_run(): fileno = {0} unregister from epoll".format(fileno))
+                        connections[fileno].close()
+                        del connections[fileno]
+        finally:
+            epoll.unregister(fd_socket.fileno())
+            epoll.close()
+            fd_socket.close()
 
 
 
 if __name__ == "__main__":
-    log_file=get_logger_file("log_operations","/var/log/server.log",logging.DEBUG)
-    first_server=Server("0.0.0.0",SERVER_PORT,log_file)
+    first_server=Server("0.0.0.0",PORT)
     first_server.start()
 
 
